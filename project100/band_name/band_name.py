43e453372ec2b25a22896209a83c41f9875329f4
Yupoer <82023047+Yupoer@users.noreply.github.com>
2021-07-08 00:10:29 +0800

    project100

---
+++ project100/band_name/band_name.py
@@ -0,0 +1,7 @@
+print("welcome to the band name generator.")
+
+city = input("which city were u grow up?:\n")
+pet = input("what is the name of ur pet?:\n")
+band = city +" "+ pet
+
+print("Here is ur band name:" + " " + band)
