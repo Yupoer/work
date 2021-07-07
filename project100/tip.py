b2281717458ee2f4c4cb9b615251d25a6b3a9809
Yupoer <82023047+Yupoer@users.noreply.github.com>
2021-07-08 00:11:34 +0800

    Delete tip.py

--- project100/tip.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator.")
-total = int(input("What wa the total bill?"))
-people = int(input("How many people to split the bill?"))
-percent_tip = float(input("What percentage tip would you like to give? 10, 12, or 15?"))/ 100
-final = ((total * percent_tip)+ total) / people
-print(f'Each person should pay: ${final:.2f}')
