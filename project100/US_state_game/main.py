e97993dba1368202fb5f2ee0801f6fa735e5526d
Yupoer <82023047+Yupoer@users.noreply.github.com>
2021-07-10 23:33:32 +0800

    project100

---
+++ project100/US_state_game/main.py
@@ -0,0 +1,40 @@
+import turtle
+
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+
+states = data.state.to_list()
+guessed_states = []
+
+def create_state():
+    state = turtle.Turtle()
+    state.hideturtle()
+    state.penup()
+    state_data = data[data["state"] == answer_state]
+    state.goto(int(state_data.x), int(state_data.y))
+    state.write(answer_state)
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 State Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_state = []
+        for state in states:
+            if state not in guessed_states:
+                missing_state.append(state)
+                new_data = pandas.DataFrame(missing_state)
+                new_data.to_csv("state_to_learn.csv")
+        break
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        create_state()
+
